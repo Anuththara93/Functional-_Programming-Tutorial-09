34f3e1ddd4a239476e17b7378dabab28c4ac4fa9
Anuththara93 <136674526+Anuththara93@users.noreply.github.com>
2023-08-22 04:33:11 +0530

    Add files via upload

---
+++ Q_1.scala
@@ -0,0 +1,19 @@
+object Q_1 {
+  def main(args: Array[String]): Unit = {
+    var x= new Rational(1,4)
+    println("Rational Number : "+x)
+
+    var y = new Rational(1, 4)
+    println("Negative Value : " + y.neg)
+  }
+  class Rational(n:Int, d:Int){
+    require(d != 0,"Denominator can not be zero")
+
+    def numerator =n
+    def denominator =d
+
+    def neg = new Rational(-this.numerator,this.denominator)
+    override def toString():String= numerator + "/" + denominator
+  }
+}
+
